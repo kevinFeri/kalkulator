070b03285bc2328e77bc57c46dfd94cfb57c4c2f
kevin <kevin.ornik@student.um.si>
2025-10-16 23:30:12 +0200

    Added potence

--- kalkulator_app/kalkulator.cpp
+++ kalkulator_app/kalkulator.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
 int izbiraFun()
@@ -9,6 +9,7 @@ int izbiraFun()
     cout << "2. -" << endl;
     cout << "3. /" << endl;
     cout << "4. *" << endl;
+    cout<<"5. ^"<<endl;
     int izbor;
     cin >> izbor;
     return izbor;
@@ -16,8 +17,8 @@ int izbiraFun()
 
 int main()
 {
-    int a;
-    int b;
+    float a;
+    float b;
     int izbira;
     cout << "Vpisite prvo stevilo: ";
     cin >> a;
@@ -39,7 +40,10 @@ int main()
             break;}
     case 4: {
                 cout << a << " * " << b << " = " << a*b << endl;
-            break;
+            break;}
+    case 5:{
+         cout<< a<<"^"<<"b"<<" = "<< pow(a,b)<<endl;
+         break;
     }
     default:
     {
